b1b3f6774979368f8aa5e855825b120ed1b427a9
captahole <captahole@gmail.com>
2025-08-06 12:02:37 -0400

    ads.txt update

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from cachetools.func import ttl_cache
 from flask import Flask, render_template, request, jsonify
 from flask import send_from_directory
 
+
 app = Flask(__name__)
+
+@app.route('/ads.txt')
+def ads():
+    return send_from_directory('.', 'ads.txt')
 
 @ttl_cache(maxsize=128, ttl=60)
 def fetch_weather(airport1, airport2, airport3=None):
